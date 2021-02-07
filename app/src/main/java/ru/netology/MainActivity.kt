001961182d3ea53afee1ceb3cfb1376d4d7331c8
Sergey Bobkov <rex55@mail.ru>
2021-02-07 12:13:44 +0300

    v 4.0

--- app/src/main/java/ru/netology/MainActivity.kt
+++ app/src/main/java/ru/netology/MainActivity.kt
@@ -6,7 +6,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.ImageView
+import android.widget.TextView
 import ru.netology.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,20 +17,50 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val  post = Post(
-                getString(R.string.sample_text),
-                liked = false
+                id = 1,
+                author = "Нетологияю Университет интернет - профессий будущего",
+                content = getString(R.string.sample_text),
+                published = "06 февраля в 20:30",
+                liked = false,
+                likes = 10,
+                toSend = false,
+                toSends = 5,
+                viewing = false,
+                viewings = 5
         )
+
+
         with(binding){
             content.text = post.content
-            imageView2.setImageResource(getImageResource(post))
 
             imageView2.setOnClickListener {
                 post.liked = !post.liked
                 imageView2.setImageResource(getImageResource(post))
+
+                if (post.liked) post.likes++ else post.likes--
+                like.text = post.likes.toString()
+            }
+
+            findViewById<ImageView>(R.id.imageView3).setOnClickListener{
+                post.toSends++
+                toSend.text = post.toSends.toString()
+            }
+
+            findViewById<ImageView>(ru.netology.R.id.imageView4).setOnClickListener{
+                post.viewings++
+                viewing.text = post.viewings.toString()
             }
         }
     }
 
+    private fun Calkulator(post: Post) {
+        if (post.liked) post.likes++ else post.likes--
+        findViewById<TextView>(R.id.like)
+
+    }
+
+
     private fun getImageResource(post: Post) =
         if (post.liked) R.drawable.ic_liked_24 else R.drawable.ic_baseline_favorite_border_24
+
 }
---
+++ app/src/main/java/ru/netology/utils/Calculator.kt
@@ -0,0 +1,8 @@
+package ru.netology.utils
+
+object Calculator {
+
+    fun convert(count: Int): String{
+        TODO()
+    }
+}
